58e94ece1a4b2ae5d0fbe409927859c390095465
Yaswanth Gundlapalli <gundlapalliyaswanth@gmail.com>
2024-08-09 18:29:29 +0530

    Add files via upload

---
+++ CmdLine1.java
@@ -0,0 +1,13 @@
+package com.cmdline;
+
+public class CmdLine1 {
+
+	public static void main(String[] args) {
+		String[] age = args;
+		for(int i = 0;i<age.length;i++) {
+			System.out.print(age[i]+ " ");
+		}
+
+	}
+
+}
